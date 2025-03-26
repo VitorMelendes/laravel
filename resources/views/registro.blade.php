52a279168251081364f5d6bd14fceb3efc9df184
VitorMelendes <vitormelendes2006@gmail.com>
2025-03-26 15:59:17 -0300

    correcao_aula_4

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -1,49 +1,7 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registro - Adote um Amigo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-      body {
-        background-color: #024059;
-        color: white;
-      }
-      .navbar {
-        background-color: #026873;
-      }
-      .register-section {
-        background-color: #04BF8A;
-        padding: 50px;
-        border-radius: 10px;
-        margin-top: 100px;
-        max-width: 500px;
-        margin-left: auto;
-        margin-right: auto;
-      }
-      .btn-primary {
-        background-color: #03A64A;
-        border-color: #03A64A;
-      }
-      .btn-primary:hover {
-        background-color: #028C3A;
-        border-color: #028C3A;
-      }
-      .form-control {
-        background-color: #025940;
-        border: 1px solid #03A64A;
-        color: white;
-      }
-      .form-control:focus {
-        background-color: #025940;
-        border-color: #03A64A;
-        color: white;
-      }
-    </style>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
+@extends('_partials/main')
+
+@section('conteudo')
+<nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">Adote um Amigo</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -88,5 +46,5 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+
+    @endsection
